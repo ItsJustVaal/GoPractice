bf0294114930bf71ed4759dc86dcf2e94aea1e88
Robert Marrett <Robmmar90@gmail.com>
2024-01-11 12:31:31 -0700

    fin

--- filenamechange.go
+++ filenamechange.go
@@ -1,68 +1,67 @@
 package main
 
-import (
-	"fmt"
-	"io/fs"
-	"os"
-	"path"
-	"path/filepath"
-)
+// import (
+// 	"fmt"
+// 	"io/fs"
+// 	"os"
+// 	"path"
+// 	"path/filepath"
+// )
 
-type file struct {
-	path string
-	name string
-	dir  string
-}
+// // type file struct {
+// // 	path string
+// // 	name string
+// // 	dir  string
+// // }
 
+// // Really basic recursive rename, I just added a
+// // word to the file name instead of parsing and changing
+// func nameChange() {
 
-// Really basic recursive rename, I just added a
-// word to the file name instead of parsing and changing
-func nameChange() {
+// 	pathArg := "sample"
+// 	fsys := os.DirFS(pathArg)
 
-	pathArg := "sample"
-	fsys := os.DirFS(pathArg)
+// 	var files []file
 
-	var files []file
+// 	fs.WalkDir(fsys, ".", func(p string, d fs.DirEntry, err error) error {
+// 		if err != nil {
+// 			fmt.Println(err.Error())
+// 		}
 
-	fs.WalkDir(fsys, ".", func(p string, d fs.DirEntry, err error) error {
-		if err != nil {
-			fmt.Println(err.Error())
-		}
+// 		if d.IsDir() {
+// 			fmt.Println("Skipping dir: ", d)
+// 		} else {
+// 			files = append(files, file{
+// 				path: filepath.Join(pathArg, p),
+// 				name: d.Name(),
+// 				dir:  path.Dir(p),
+// 			})
+// 		}
 
-		if d.IsDir() {
-			fmt.Println("Skipping dir: ", d)
-		} else {
-			files = append(files, file{
-				path: filepath.Join(pathArg, p),
-				name: d.Name(),
-				dir:  path.Dir(p),
-			})
-		}
+// 		return nil
+// 	})
+// 	for _, file := range files {
+// 		newFile := buildNewPath(file, pathArg)
+// 		err := os.Rename(file.path, newFile.path)
+// 		if err != nil {
+// 			fmt.Println(err.Error())
+// 		}
+// 	}
+// }
 
-		return nil
-	})
-	for _, file := range files {
-		newFile := buildNewPath(file, pathArg)
-		err := os.Rename(file.path, newFile.path)
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-	}
-}
+// func buildNewPath(f file, root string) file {
+// 	if f.dir == "." {
+// 		return file{
+// 			name: f.name,
+// 			path: filepath.Join(root, f.name),
+// 			dir:  f.dir,
+// 		}
+// 	} else {
+// 		return file{
+// 			name: f.name,
+// 			path: filepath.Join(root, f.dir, f.name),
+// 			dir:  f.dir,
+// 		}
+// 	}
 
-func buildNewPath(f file, root string) file {
-	if f.dir == "." {
-		return file{
-			name: f.name,
-			path: filepath.Join(root, f.name),
-			dir:  f.dir,
-		}
-	} else {
-		return file{
-			name: f.name,
-			path: filepath.Join(root, f.dir, f.name),
-			dir:  f.dir,
-		}
-	}
-
-}
+// }
