9ca21c91f4f4212cb1d9a35a2e6cfcd2344248d1
Robert Marrett <Robmmar90@gmail.com>
2024-04-22 17:21:08 -0600

    Stubbed out the blackjack AI package

--- blackjack/ai.go
+++ blackjack/ai.go
@@ -1,11 +1,53 @@
 package blackjack
 
-import "github.com/ItsJustVaal/GoPractice/deck"
+import (
+	"fmt"
 
-type AI interface{}
+	"github.com/ItsJustVaal/GoPractice/deck"
+)
+
+type AI interface {
+	Bet() int
+	Play(hand []deck.Card, dealer deck.Card) Move
+	Results(hand [][]deck.Card, dealer []deck.Card)
+}
 
 type HumanAI struct{}
 
-func (ai *HumanAI) Play(hand []deck.Card) {
+func (ai *HumanAI) Bet() int {
+	return 1
+}
 
+func (ai *HumanAI) Play(hand []deck.Card, dealer deck.Card) Move {
+	for {
+		fmt.Println("Player Hand: ", hand)
+		fmt.Println("Dealer Hand: ", dealer)
+		fmt.Println("Would you like to (h)it or (s)tand?")
+		var input string
+		fmt.Scanf("%s\n", &input)
+		switch input {
+		case "h":
+			return Hit
+		case "s":
+			return Stand
+		default:
+			fmt.Println("Invalid Option")
+		}
+	}
+}
+
+func (ai *HumanAI) Results(hand [][]deck.Card, dealer []deck.Card) {
+
+}
+
+type Move func(GameState) GameState
+
+type GameState struct{}
+
+func Hit(gs GameState) GameState {
+	return gs
+}
+
+func Stand(gs GameState) GameState {
+	return gs
 }
